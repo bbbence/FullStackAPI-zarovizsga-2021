53c5872df03a3f710f76db28fafb9690907da3fb
Bence Borbás <benceborbas88@gmail.com>
2021-08-09 14:04:05 +0200

    Exercise 1.

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,33 @@
 // 1. Feladat: pótold a service modellt.
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: 'Hungary'
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('service', ServiceSchema);
